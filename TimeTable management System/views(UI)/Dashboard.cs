6d456e87d7c5e86d6dc26fd8a73f7e32e544e711
GayathriVidarshana <karandhanagayathri@gmail.com>
2021-03-27 10:13:39 +0530

    Add project files.

---
+++ TimeTable management System/views(UI)/Dashboard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeTable_management_System.views_UI_
+{
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+    }
+}
